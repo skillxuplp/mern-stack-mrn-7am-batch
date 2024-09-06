6ea7fb9cc256a64769fc3c85e11196bf9f40716b
skillxup <skillxup@gmail.com>
2024-09-06 22:22:23 +0530

    reactjs

--- reactjs/01. React Essentials/15-deriving-computing-values/src/components/CoreConcept.jsx
+++
@@ -1,9 +0,0 @@
-export default function CoreConcept({title, description, image}) {
-    return(
-      <li>
-        <img src={image} alt={title} />
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </li>
-    );
-}
